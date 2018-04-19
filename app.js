81bd9e5dc296f65e7a76ef226c3f44a855139ae9
Jonathan Brandwein <jonathan.brandwein@gmail.com>
2018-04-19 18:49:03 -0400

    index.html

--- app.js
+++ app.js
@@ -8,6 +8,4 @@ app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
 
-
-
 app.listen(process.env.PORT || 3000);
